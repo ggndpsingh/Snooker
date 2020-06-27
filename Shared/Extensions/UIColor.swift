2a17d7b37b49ff34fe800947e125e6155f51e0bf
Gagandeep Singh <gagan.deep@me.com>
2020-06-27 18:01:34 +1000

    Extracted all views

---
+++ Shared/Extensions/UIColor.swift
@@ -0,0 +1,21 @@
+//  Copyright © 2020 DeepGagan. All rights reserved.
+
+import SwiftUI
+
+public extension Color {
+    static let lightText = Color(UIColor.lightText)
+    static let darkText = Color(UIColor.darkText)
+
+    static let label = Color(UIColor.label)
+    static let secondaryLabel = Color(UIColor.secondaryLabel)
+    static let tertiaryLabel = Color(UIColor.tertiaryLabel)
+    static let quaternaryLabel = Color(UIColor.quaternaryLabel)
+
+    static let systemBackground = Color(UIColor.systemBackground)
+    static let secondarySystemBackground = Color(UIColor.secondarySystemBackground)
+    static let tertiarySystemBackground = Color(UIColor.tertiarySystemBackground)
+
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+    static let tertiarySystemGroupedBackground = Color(UIColor.tertiarySystemGroupedBackground)
+}
--- Shared/GameView.swift
+++ Shared/GameView.swift
@@ -6,59 +6,15 @@ struct GameView: View {
     @ObservedObject var game: Game
     
     var body: some View {
-        VStack {
-            HStack() {
-                PlayerView(player: game.playerOne, isActive: game.activePlayer == game.playerOne)
+        ZStack {
+            Color.secondarySystemBackground
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                PlayersView(playerOne: game.playerOne, playerTwo: game.playerTwo, activePlayerID: game.activePlayer.id)
+                BallsView(potAction: game.pot)
                 Spacer()
-                PlayerView(player: game.playerTwo, isActive: game.activePlayer == game.playerTwo)
+                SwitchPlayerView(switchPlayerHandler: game.switchPlayer)
             }
-            Spacer()
-            VStack(spacing: 16) {
-                BallView(ball: .red) {
-                    game.pot(.red)
-                }
-                
-                HStack {
-                    ForEach([Ball.yellow, Ball.green, Ball.brown], id: \.self) { ball in
-                        BallView(ball: ball) {
-                            self.game.pot(ball)
-                        }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
-                    }
-                }
-                
-                HStack {
-                    ForEach([Ball.blue, Ball.pink, Ball.black], id: \.self) { ball in
-                        BallView(ball: ball) {
-                            self.game.pot(ball)
-                        }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
-                    }
-                }
-            }
-            .padding()
-            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 320, alignment: .center)
-            
-            Spacer()
-            
-            Button(action: {
-                self.game.switchPlayer()
-            }) {
-                Image(systemName: "arrow.right.arrow.left")
-            }
-            .frame(width: 60, height: 60, alignment: .center)
-            .overlay(
-                Circle()
-                    .stroke(Color.blue,lineWidth: 2)
-            ).foregroundColor(Color.blue)
-        }
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        Group {
-            GameView(game: .testGame)
-            GameView(game: .testGame)
-                .preferredColorScheme(.dark)
         }
     }
 }
@@ -70,49 +26,111 @@ struct PlayerView: View {
     var body: some View {
         VStack(spacing: 8) {
             Text(player.name)
+                .font(Font.system(size: 14, weight: .medium, design: .rounded))
             ScoreView(player: player, isActive: isActive)
         }
         .padding()
     }
+    
+    struct ScoreView: View {
+        @ObservedObject var player: Player
+        let isActive: Bool
+        
+        var body: some View {
+            Text("\(player.score)")
+                .font(Font.system(size: 32, weight: .bold, design: .rounded))
+                .foregroundColor(.white)
+                .frame(width: 100, height: 100)
+                .background(background)
+                .clipShape(
+                    RoundedRectangle(cornerRadius: 12, style:.continuous))
+        }
+        
+        private var background: Color {
+            isActive ? .green : .gray
+        }
+    }
 }
 
-struct ScoreView: View {
-    @ObservedObject var player: Player
-    let isActive: Bool
+struct PlayersView: View {
+    @ObservedObject var playerOne: Player
+    @ObservedObject var playerTwo: Player
+    let activePlayerID: String
     
     var body: some View {
-        Text("\(player.score)")
-            .font(Font.system(size: 32, weight: .medium, design: .rounded))
-            .foregroundColor(.white)
-            .frame(width: 100, height: 100)
-            .background(background)
-            .clipShape(
-                RoundedRectangle(cornerRadius: 12, style:.continuous))
-    }
-    
-    private var background: Color {
-        isActive ? .green : .gray
+        HStack() {
+            PlayerView(player: playerOne, isActive: playerOne.id == activePlayerID)
+            Spacer()
+            PlayerView(player: playerTwo, isActive: playerTwo.id == activePlayerID)
+        }
     }
 }
 
 struct BallView: View {
     let ball: Ball
-    let action: () -> Void
+    let potAction: (Ball) -> Void
     
     var body: some View {
         Button(action: {
-            action()
+            potAction(ball)
         }) {
             Text("\(ball.rawValue)")
-                .font(Font.system(size: 24, weight: .semibold, design: .rounded))
+                .font(Font.system(size: 24, weight: .bold, design: .rounded))
                 .foregroundColor(Color.white)
-                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, idealHeight: 64, maxHeight: 64, alignment: .center)
         }
         .background(ball.color)
         .clipShape(
             RoundedRectangle(cornerRadius: 12, style:.continuous))
+    }
+}
+
+struct BallsRowView: View {
+    let balls: [Ball]
+    let potAction: (Ball) -> Void
+    var body: some View {
+        HStack {
+            ForEach(balls, id: \.self) { ball in
+                BallView(ball: ball, potAction: potAction)
+            }
+        }
+    }
+}
+
+struct BallsView: View {
+    let potAction: (Ball) -> Void
+    var body: some View {
+        VStack(spacing: 16) {
+            BallView(ball: .red, potAction: potAction)
+            BallsRowView(balls: [.yellow, .green, .brown], potAction: potAction)
+            BallsRowView(balls: [.blue, .pink, .black], potAction: potAction)
+        }
+        .padding()
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 260, alignment: .center)
+    }
+}
+
+struct SwitchPlayerView: View {
+    let switchPlayerHandler: () -> Void
+    var body: some View {
+        Button(action: switchPlayerHandler) {
+            Image(systemName: "arrow.right.arrow.left")
+        }
+        .frame(width: 60, height: 60, alignment: .center)
         .overlay(
-            RoundedRectangle(cornerRadius: 12, style:.continuous)
-                .stroke(Color.primary, lineWidth: ball == .black ? 1 : 0))
+            Circle()
+                .stroke(Color.blue,lineWidth: 2)
+        ).foregroundColor(Color.blue)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            GameView(game: .testGame)
+//            GameView(game: .testGame)
+//                .previewDevice("iPad Pro (12.9-inch) (4th generation)")
+//                .preferredColorScheme(.dark)
+        }
     }
 }
